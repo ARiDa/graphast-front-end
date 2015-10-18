2dee7f8ab5f86e0b7a9bf804736b2d7a7669c2f8
igobrilhante <brilhanteigo@gmail.com>
2015-10-18 12:17:37 -0300

    Add first stuff about map

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # graphast-front-end
 Graphast Front End
+
+
+To create a simple web server:
+
+```
+python -m SimpleHTTPServer 8000
+```
---
+++ js/map.js
@@ -0,0 +1,64 @@
+(function(L){
+var ACCESS_TOKEN = 'pk.eyJ1IjoibWFwYm94IiwiYSI6IjZjNmRjNzk3ZmE2MTcwOTEwMGY0MzU3YjUzOWFmNWZhIn0.Y8bhBaUMqFiPrDRW9hieoQ';
+var MB_URL = 'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=' + ACCESS_TOKEN;
+
+L.mapbox.accessToken = ACCESS_TOKEN;
+
+var map = L.mapbox.map('map', 'mapbox.light')
+  .setView([-11.505, -41.09], 2);
+
+var marker = L.marker([-11.505, -41.09], {
+    icon: L.mapbox.marker.icon({
+    	'marker-size': 'large',
+       	'marker-symbol': 'bus',
+      	'marker-color': '#f86767'
+    })
+});
+
+var t = -41.09;
+var t2 = -11.505
+window.setInterval(function() {
+    // Making a lissajous curve just for fun.
+    // Create your own animated path here.
+    marker.setLatLng(L.latLng(
+        t2 * 1.1,
+        t * 1.1));
+    t += 1;
+    t2 += 1;
+}, 1000);
+
+marker.addTo(map);
+
+// Add a new line to the map with no points.
+var polyline = L.polyline([]).addTo(map);
+
+// Keep a tally of how many points we've added to the map.
+var pointsAdded = 0;
+
+// Start drawing the polyline.
+add();
+
+function add() {
+
+    // `addLatLng` takes a new latLng coordinate and puts it at the end of the
+    // line. You optionally pull points from your data or generate them. Here
+    // we make a sine wave with some math.
+    polyline.addLatLng(
+        L.latLng(
+            Math.cos(pointsAdded / 20) * 30,
+            pointsAdded));
+
+    // Pan the map along with where the line is being added.
+    // map.setView([0, pointsAdded], 3);
+
+    // Continue to draw and pan the map by calling `add()`
+    // until `pointsAdded` reaches 360.
+    if (++pointsAdded < 360) window.setTimeout(add, 100);
+}
+
+var GraphastMap = {
+	addPath: {},
+	addPoint: {}
+}
+
+})(L)
