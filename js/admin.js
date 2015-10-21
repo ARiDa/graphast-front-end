398308db49352fdca918e9f975eaf28f584d18ab
Josueh <josueh.cg@gmail.com>
2015-10-20 22:25:30 -0300

    sidebar admin screen

--- js/admin.js
+++ js/admin.js
@@ -1,5 +1,12 @@
 
 function adminInit() {
+	BtnMenuClose.addEventListener('click', function() {
+		document.body.classList.toggle('admin')
+		window['isAdmin'] = document.body.classList.contains('admin')
+		//
+		if (isAdmin)
+			;// ...
+	})
 	// setInterval(function() {
 	// 	document.body.classList.toggle('admin')
 	// }, 3000)
