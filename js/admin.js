ddfd393d566367a870eda83b307dbb8ea8756a97
Josueh <josueh.cg@gmail.com>
2015-10-21 21:25:56 -0300

    admin sidebar filter

--- js/admin.js
+++ js/admin.js
@@ -1,5 +1,7 @@
 
 function adminInit() {
+	//
+	// event click ||| menu (top left)
 	BtnMenuClose.addEventListener('click', function() {
 		document.body.classList.toggle('admin')
 		window['isAdmin'] = document.body.classList.contains('admin')
@@ -7,6 +9,40 @@ function adminInit() {
 		if (isAdmin)
 			;// ...
 	})
+	//
+	// searching/filtering apps
+	Search.addEventListener('keyup', function() {
+		var q = this.value.toLowerCase()
+		var items = document.querySelectorAll('#AppsResult button')
+		for (var i = 0; i < items.length; i++) {
+			if (items[i].innerHTML.toLowerCase().indexOf(q) == -1)
+				items[i].classList.add('hidden')
+			else
+				items[i].classList.remove('hidden')
+			items[i].classList.remove('selected')
+		}
+	})
+	//
+	// click on app buttons (edit app)
+	AppsResult.addEventListener('click', function(e) {
+		var btn = e.target
+		if (btn.tagName == 'DIV')
+			return
+		if (btn.tagName == 'SPAN')
+			btn = btn.parentNode
+		if (btn.tagName == 'B')
+			btn = btn.parentNode
+		if (btn.tagName != 'BUTTON')
+			return // click in invalid element
+		var items = document.querySelectorAll('#AppsResult button')
+		for (var i = 0; i < items.length; i++)
+			items[i].classList.remove('selected')
+		btn.classList.add('selected')
+	})
+	//
+
+
+
 	// setInterval(function() {
 	// 	document.body.classList.toggle('admin')
 	// }, 3000)
