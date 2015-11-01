d3a07c289d9718dc65bfa7c0837d61fcc5764e34
Josueh <josueh.cg@gmail.com>
2015-10-31 23:07:23 -0300

    add clear pois in dialog osr

--- js/dialog-osr.js
+++ js/dialog-osr.js
@@ -13,25 +13,32 @@ function createDialogOSR() {
 	pois[105] = 'Supermarket'
 	pois[162] = 'Fuel'
 	//
-	var htmlPois = ''
+	var html = ''
 	for (var id in pois)
-		htmlPois += '<b data-id="'+id+'">' + pois[id] + '</b>'
-	htmlPois = '<div>' + htmlPois + '</div>'
-
-	$('#add-osr-poi').on('click', 'button', function() {
-		$('#add-osr-poi').html(htmlPois)
-		return false
+		html += '<b data-id="'+id+'">'+pois[id]+'</b>'
+	html = '<div>'+html+'</div>'
+	//
+	$('.dialog-osr .add').on('click', 'button', function() {
+		$('#add-osr-poi').html(html)
+	})
+	$('.dialog-osr .places').on('click', 'button', function() {
+		$('.dialog-osr .places').empty()
 	})
 	$('#add-osr-poi').on('click', 'div b', function(e) {
-		var item = '<p><span>' + e.target.innerHTML + '</span></p>'
-		$('.dialog-osr .pois .places').append(item)
+		var places = $('.dialog-osr .pois .places')
+		if (places.children().length == 0)
+			places.append('<p><button>x</button></p>')
+		var id   = e.target.getAttribute('data-id')
+		var name = e.target.innerHTML
+		var item = '<p><span data-id="'+id+'">'+name+'</span></p>'
+		places.append(item)
 		$('#add-osr-poi').html('<button>+</button>')
 	})
 	//
 	// get current POIs selected by user
 	window.getCurrentPOIs = function() {
 		var result = []
-		$('.dialog-osr .pois .places b').each(function() {
+		$('.dialog-osr .pois .places span').each(function() {
 			result.push(+$(this).attr('data-id'))
 		})
 		return result
