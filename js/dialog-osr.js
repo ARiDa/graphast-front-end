d8d50c25b304244b3b11bec29d2ce24f1e4f2673
Josueh <josueh.cg@gmail.com>
2015-10-31 22:11:20 -0300

    update pois

--- js/dialog-osr.js
+++ js/dialog-osr.js
@@ -1,22 +1,21 @@
 
 function createDialogOSR() {
-	var pois = [
-		'bank',
-		'beach',
-		'restaurant',
-		'shopping',
-		'abc',
-		'cinema',
-		'gym',
-		'hospital',
-		'park',
-		'theatre',
-		'museum',
-		'airport'
-	]
+	var pois  = {}
+	pois[6]   = 'Hotel'
+	pois[13]  = 'Police'
+	pois[22]  = 'University'
+	pois[23]  = 'Bar'
+	pois[25]  = 'Cafe'
+	pois[29]  = 'Restaurant'
+	pois[33]  = 'Hospital'
+	pois[34]  = 'Pharmacy'
+	pois[46]  = 'Bank'
+	pois[105] = 'Supermarket'
+	pois[162] = 'Fuel'
+	//
 	var htmlPois = ''
-	for (var i = 0; i < pois.length; i++)
-		htmlPois += '<b>' + pois[i] + '</b>'
+	for (var id in pois)
+		htmlPois += '<b data-id="'+id+'">' + pois[id] + '</b>'
 	htmlPois = '<div>' + htmlPois + '</div>'
 
 	$('#add-osr-poi').on('click', 'button', function() {
@@ -28,6 +27,15 @@ function createDialogOSR() {
 		$('.dialog-osr .pois .places').append(item)
 		$('#add-osr-poi').html('<button>+</button>')
 	})
+	//
+	// get current POIs selected by user
+	window.getCurrentPOIs = function() {
+		var result = []
+		$('.dialog-osr .pois .places b').each(function() {
+			result.push(+$(this).attr('data-id'))
+		})
+		return result
+	}
 }
 
 
