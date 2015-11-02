459b9013f9b0c6868eec10d814d984b657e35377
Josueh <josueh.cg@gmail.com>
2015-11-02 09:38:48 -0300

    add kNN dialog

--- js/app-menu.js
+++ js/app-menu.js
@@ -7,11 +7,18 @@ function createAppMenu() {
 		var button = $(this).html()
 		if (button.indexOf('Shortest Path') == 0) {
 			$('.dialog.dialog-osr').addClass('hidden')
+			$('.dialog.dialog-knn').addClass('hidden')
 			$('.dialog.dialog-shortest-path').removeClass('hidden')
 		}
 		if (button.indexOf('OSR') == 0) {
 			$('.dialog.dialog-shortest-path').addClass('hidden')
+			$('.dialog.dialog-knn').addClass('hidden')
 			$('.dialog.dialog-osr').removeClass('hidden')
+		}
+		if (button.indexOf('kNN') == 0) {
+			$('.dialog.dialog-shortest-path').addClass('hidden')
+			$('.dialog.dialog-osr').addClass('hidden')
+			$('.dialog.dialog-knn').removeClass('hidden')
 		}
 	})
 }
