bed81f01887a1063148efd73c65ece9dbca92129
Josué Machado <josueh.developer@gmail.com>
2015-11-04 09:09:33 -0300

    join login screen with service

--- js/login.js
+++ js/login.js
@@ -7,24 +7,11 @@ function createLogin() {
 	$('#Login input').keypress(function(e) {
 		if (e.keyCode == 13) {
 			var s = $('#Login input').val()
-			if (s == '123') {
+			$.post('http://demo.graphast.org:8080/graphast-ws/login', {pass: s}, function(data) {
 				$('body').removeClass('public')
 				$('#Login').addClass('hidden')
-				location.hash = ''
-			} else {
-				$('#Login input').val('')
-				$('#Login input')
-					.animate({marginLeft: "-=40px"}, 100)
-					.animate({marginLeft: "+=80px"}, 200)
-					.animate({marginLeft: "-=80px"}, 200)
-					.animate({marginLeft: "+=80px"}, 200)
-					.animate({marginLeft: "-=40px"}, 100)
-			}
-			return
-			//
-			$.post('/login', {pass: $('#Login input').val()}, function(data) {
-				$('body').addClass('superuser')
-			}, function(err) {
+				location.href = '/'
+			}).error(function(err) {
 				$('#Login input').val('')
 				$('#Login input')
 					.animate({marginLeft: "-=40px"}, 100)
